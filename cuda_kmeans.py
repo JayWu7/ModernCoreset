15c5182d6d5e2f9ad4019316e65c6525308f60a2
JayWu7 <jaywu16@163.com>
2021-01-14 12:56:23 +0200

    write a small cuda-kmeans script on gdelt data

--- cuda_kmeans.py
+++ cuda_kmeans.py
@@ -1,30 +1,36 @@
 # Both import methods supported
 from cuml import KMeans
 from cuml.cluster import KMeans
+from dataloader import loader
 
 import cudf
 import numpy as np
 import pandas as pd
 
+
 def np2cudf(df):
     # convert numpy array to cuDF dataframe
-    df = pd.DataFrame({'fea%d'%i:df[:,i] for i in range(df.shape[1])})
+    df = pd.DataFrame({'fea%d' % i: df[:, i] for i in range(df.shape[1])})
     pdf = cudf.DataFrame()
-    for c,column in enumerate(df):
-      pdf[str(c)] = df[column]
+    for c, column in enumerate(df):
+        pdf[str(c)] = df[column]
     return pdf
 
-a = np.asarray([[1.0, 1.0], [1.0, 2.0], [3.0, 2.0], [4.0, 3.0]],
-               dtype=np.float32)
-b = np2cudf(a)
-print("input:")
-print(b)
 
-print("Calling fit")
-kmeans_float = KMeans(n_clusters=2)
-kmeans_float.fit(b)
 
-print("labels:")
-print(kmeans_float.labels_)
-print("cluster_centers:")
-print(kmeans_float.cluster_centers_)
+def cuml_kmeans():
+    a = np.asarray([[1.0, 1.0], [1.0, 2.0], [3.0, 2.0], [4.0, 3.0]],
+                   dtype=np.float32)
+    raw_data = loader(filename='gdelt', specific_file='20200513.gkgcounts.csv', sep='\t')
+    data = np2cudf(raw_data)
+    print("input:")
+    print(data)
+
+    print("Calling fit")
+    kmeans_float = KMeans(n_clusters=2)
+    kmeans_float.fit(data)
+
+    print("labels:")
+    print(kmeans_float.labels_)
+    print("cluster_centers:")
+    print(kmeans_float.cluster_centers_)
